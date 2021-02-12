5f2104722ad40980c24df3b3def0b116d7c68bdd
Juan Felipe Blanco <juanfeblancot@gmail.com>
2021-02-12 18:19:53 -0500

    Login window now displays

---
+++ src/ui/ClassroomGUI.java
@@ -0,0 +1,13 @@
+package ui;
+
+import model.Classroom;
+
+public class ClassroomGUI {
+
+	private Classroom classroom;
+	
+	public ClassroomGUI(Classroom classroom) {
+		this.classroom = classroom;
+	}
+
+}
